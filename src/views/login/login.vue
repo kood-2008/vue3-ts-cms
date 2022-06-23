656de85e768f79be325a446b65c4173db1265ce1
kood-2008 <maabc2008@126.com>
2022-06-23 20:29:44 +0800

    docs: router
    install router

---
+++ src/views/login/login.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: 描述
+ * @version: 1.0.0
+ * @Author: xiaoma
+ * @Date: 2022-06-23 20:03:55
+ * @LastEditors: xiaoma
+ * @LastEditTime: 2022-06-23 20:03:55
+ -->
+<template>
+  <div class="">Login</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/main/main.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: 描述
+ * @version: 1.0.0
+ * @Author: xiaoma
+ * @Date: 2022-06-23 20:04:08
+ * @LastEditors: xiaoma
+ * @LastEditTime: 2022-06-23 20:04:08
+ -->
+<template>
+  <div class="">Main</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
